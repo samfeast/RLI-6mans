a7c217fbbe1861da7bd2dfb32f3998596ee07e62
Sam Feast <sfeast43@gmail.com>
2022-06-27 23:24:02 +0100

    corrected responses for manual/forced entry

--- queue_handler_cog.py
+++ queue_handler_cog.py
@@ -2,6 +2,7 @@ import discord
 from discord.ext import commands
 from discord import app_commands
 import json
+import random
 
 with open("json/config.json", "r") as read_file:
     config = json.load(read_file)
@@ -16,11 +17,23 @@ championship_logs_channel_id = config["tiers"]["championship_logs"]
 casual_logs_channel_id = config["tiers"]["casual_logs"]
 
 elite_queue = []
-premier_queue = []
+premier_queue = [
+    297085754658652172,
+    495542213535858693,
+    209776204817891328,
+    202118945803730944,
+    201478097667751936,
+]
 championship_queue = []
 casual_queue = []
 
-all_tier_queue = []
+all_tier_queue = [
+    297085754658652172,
+    495542213535858693,
+    209776204817891328,
+    202118945803730944,
+    201478097667751936,
+]
 
 
 class queue_handler(commands.Cog):
@@ -42,15 +55,33 @@ class queue_handler(commands.Cog):
             )
         else:
             if interaction.channel_id == elite_channel_id:
-                await self.add_to_queue(interaction, interaction.user, elite_queue)
+                await self.add_to_queue(
+                    interaction, interaction.user, elite_queue, elite_channel_id, False
+                )
             elif interaction.channel_id == premier_channel_id:
-                await self.add_to_queue(interaction, interaction.user, premier_queue)
+                await self.add_to_queue(
+                    interaction,
+                    interaction.user,
+                    premier_queue,
+                    premier_channel_id,
+                    False,
+                )
             elif interaction.channel_id == championship_channel_id:
                 await self.add_to_queue(
-                    interaction, interaction.user, championship_queue
+                    interaction,
+                    interaction.user,
+                    championship_queue,
+                    championship_channel_id,
+                    False,
                 )
             elif interaction.channel_id == casual_channel_id:
-                await self.add_to_queue(interaction, interaction.user, casual_queue)
+                await self.add_to_queue(
+                    interaction,
+                    interaction.user,
+                    casual_queue,
+                    casual_channel_id,
+                    False,
+                )
             else:
                 await interaction.response.send_message(
                     "Queuing is not enabled in this channel."
@@ -66,25 +97,41 @@ class queue_handler(commands.Cog):
             )
         else:
             if interaction.channel_id == elite_logs_channel_id:
-                await self.add_to_queue(interaction, user, elite_queue)
+                await self.add_to_queue(
+                    interaction, user, elite_queue, elite_channel_id, True
+                )
             elif interaction.channel_id == premier_logs_channel_id:
-                await self.add_to_queue(interaction, user, premier_queue)
+                await self.add_to_queue(
+                    interaction, user, premier_queue, premier_channel_id, True
+                )
             elif interaction.channel_id == championship_logs_channel_id:
-                await self.add_to_queue(interaction, user, championship_queue)
+                await self.add_to_queue(
+                    interaction, user, championship_queue, championship_channel_id, True
+                )
             elif interaction.channel_id == casual_logs_channel_id:
-                await self.add_to_queue(interaction, user, casual_queue)
+                await self.add_to_queue(
+                    interaction, user, casual_queue, casual_channel_id, True
+                )
             else:
                 await interaction.response.send_message(
                     "Adding and removing players from the queue is not enabled in this channel."
                 )
 
-    async def add_to_queue(self, interaction, user, queue):
+    async def add_to_queue(self, interaction, user, queue, channel_id, added):
+        tier_channel = self.bot.get_channel(channel_id)
         queue.append(user.id)
         all_tier_queue.append(user.id)
-        if len(queue) == 6:
-            await interaction.response.send_message(f"Queue popped: {queue}")
+        if added:
+            await interaction.response.send_message(f"{user.name} added to queue.")
+            await tier_channel.send(f"{user.name} has joined the queue.")
         else:
-            await interaction.response.send_message(f"{user.name} has joined the queue")
+            await interaction.response.send_message(
+                f"{user.name} has joined the queue."
+            )
+        if len(queue) == 6:
+            await tier_channel.send(f"Queue popped: {queue}")
+            random_queue = await self.random_teams(queue)
+            await tier_channel.send(f"Teams: {random_queue}")
 
     @app_commands.command(description="Leave the queue.")
     @app_commands.guilds(discord.Object(id=846538497087111169))
@@ -127,6 +174,13 @@ class queue_handler(commands.Cog):
             await interaction.response.send_message(f"{user.name} has left the queue")
         except ValueError:
             await interaction.response.send_message("User is not in the queue")
+
+    async def random_teams(self, queue):
+        print(queue)
+        random.shuffle(queue)
+        print(queue)
+
+        return queue
 
 
 async def setup(bot):
