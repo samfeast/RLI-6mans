fbf405ec7971130f0a4fe7f0a48afb860d8276cf
Sam Feast <sfeast43@gmail.com>
2022-07-03 13:17:30 +0100

    leaderboard position on stats

--- stats_cog.py
+++ stats_cog.py
@@ -83,7 +83,26 @@ class stats(commands.Cog):
                 name="Win Percentage:",
                 value=f"{(player_data[tier][str(user.id)]['wins']/(player_data[tier][str(user.id)]['wins'] + player_data[tier][str(user.id)]['losses']))*100:.2f}%",
             )
-            player_embed.add_field(name="Leaderboard Pos:", value=f"x/x")
+            leaderboard_dict = {}
+            for player in player_data[tier]:
+                leaderboard_dict[player] = player_data[tier][player]["points"]
+
+            print(leaderboard_dict)
+
+            i = 1
+
+            for k, v in sorted(
+                leaderboard_dict.items(), key=lambda item: item[1], reverse=True
+            ):
+                if k == str(user.id):
+                    break
+
+                i += 1
+
+            player_embed.add_field(
+                name="Leaderboard Pos:",
+                value=f"{i}/{len(leaderboard_dict)}",
+            )
 
         await interaction.response.send_message(embed=player_embed)
 
@@ -95,22 +114,55 @@ class stats(commands.Cog):
 
         if tier == None:
             if str(interaction.user.id) in player_data["elite"]:
-                await self.show_leaderboard(interaction, player_data, "elite")
+                await self.show_leaderboard(interaction, player_data, "elite", True)
             elif str(interaction.user.id) in player_data["premier"]:
-                await self.show_leaderboard(interaction, player_data, "premier")
+                await self.show_leaderboard(interaction, player_data, "premier", True)
             elif str(interaction.user.id) in player_data["championship"]:
-                await self.show_leaderboard(interaction, player_data, "championship")
+                await self.show_leaderboard(
+                    interaction, player_data, "championship", True
+                )
             elif str(interaction.user.id) in player_data["casual"]:
-                await self.show_leaderboard(interaction, player_data, "casual")
+                await self.show_leaderboard(interaction, player_data, "casual", True)
             else:
                 await interaction.response.send_message("Tier not found")
         else:
             try:
-                await self.show_leaderboard(interaction, player_data, tier.lower())
+                await self.show_leaderboard(
+                    interaction, player_data, tier.lower(), True
+                )
             except:
                 await interaction.response.send_message("Unable to display stats.")
 
-    async def show_leaderboard(self, interaction, player_data, tier):
+    @app_commands.command(description="Show the reverse leaderboard.")
+    @app_commands.guilds(discord.Object(id=846538497087111169))
+    async def reverse_leaderboard(
+        self, interaction: discord.Interaction, tier: str = None
+    ):
+        with open("json/player_data.json", "r") as read_file:
+            player_data = json.load(read_file)
+
+        if tier == None:
+            if str(interaction.user.id) in player_data["elite"]:
+                await self.show_leaderboard(interaction, player_data, "elite", False)
+            elif str(interaction.user.id) in player_data["premier"]:
+                await self.show_leaderboard(interaction, player_data, "premier", False)
+            elif str(interaction.user.id) in player_data["championship"]:
+                await self.show_leaderboard(
+                    interaction, player_data, "championship", False
+                )
+            elif str(interaction.user.id) in player_data["casual"]:
+                await self.show_leaderboard(interaction, player_data, "casual", False)
+            else:
+                await interaction.response.send_message("Tier not found")
+        else:
+            try:
+                await self.show_leaderboard(
+                    interaction, player_data, tier.lower(), False
+                )
+            except:
+                await interaction.response.send_message("Unable to display stats.")
+
+    async def show_leaderboard(self, interaction, player_data, tier, reverse):
 
         leaderboard_dict = {}
         for player in player_data[tier]:
@@ -122,15 +174,27 @@ class stats(commands.Cog):
             title=f"{tier.capitalize()} Leaderboard", color=0x83FF00
         )
         i = 1
-        for k, v in sorted(
-            leaderboard_dict.items(), key=lambda item: item[1], reverse=True
-        ):
-            leaderboard_embed.add_field(
-                name=f"{i}:",
-                value=f"{self.bot.get_user(int(k)).name}: {v:.2f}",
-                inline=False,
-            )
-            i += 1
+
+        if reverse == True:
+            for k, v in sorted(
+                leaderboard_dict.items(), key=lambda item: item[1], reverse=True
+            ):
+                leaderboard_embed.add_field(
+                    name=f"{i}:",
+                    value=f"{self.bot.get_user(int(k)).name}: {v:.2f}",
+                    inline=False,
+                )
+                i += 1
+        elif reverse == False:
+            for k, v in sorted(
+                leaderboard_dict.items(), key=lambda item: item[1], reverse=False
+            ):
+                leaderboard_embed.add_field(
+                    name=f"{len(leaderboard_dict) + 1 - i}:",
+                    value=f"{self.bot.get_user(int(k)).name}: {v:.2f}",
+                    inline=False,
+                )
+                i += 1
 
         await interaction.response.send_message(embed=leaderboard_embed)
 
